66a456ae1734c7cb99d7eba0cca4a3ebb5793702
mk0910-afk <152643948+mk0910-afk@users.noreply.github.com>
2024-12-23 14:57:43 +0000

    basics On

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,28 @@
 console.log("hello")
+
+let name = "manas"
+let age = 18 
+let subject
+let college = null
+
+
+console.table([name , age , subject ,college]);
+
+// avoid using var , instead use let , 
+
+/*
+variable
+number 2^253
+bigint
+string 
+boolean
+null => standalone value
+undefined
+symbol =>unique
+
+
+object
+*/
+
+console.log(typeof null) //object
+console.log(typeof undefined ) //undefined
