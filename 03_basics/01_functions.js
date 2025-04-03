00e50a0ff897c35509a7793f95f8c210a9bfdff9
mk0910-afk <152643948+mk0910-afk@users.noreply.github.com>
2025-04-03 20:04:40 +0000

    func 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -10,7 +10,7 @@ function add(num1 , num2){
     return num1 + num2;
 }
 
-console.log(add(3 ,4));
+//console.log(add(3 ,4));
 // when u cant pass an argument by defualt prints "undefined"
 
 function loginMessage(username = "man"){
@@ -22,4 +22,37 @@ function loginMessage(username = "man"){
 
     return `${username} jus logged in`;
 }
-console.log(loginMessage("manas"));
+//console.log(loginMessage("manas"));
+
+
+function calculateCartPrice(val1 , val2 , ...num1){ // ... this is called rest and spread operator depending upon usecase
+    return num1;
+}
+
+//console.log(calculateCartPrice(200 , 100 , 400 , 3000));
+
+const user = {
+    username : "manas",
+    price : 99
+}
+
+function handleObject(anyobject){
+    console.log(`user is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+
+handleObject(user);
+handleObject({
+    username : "sam" , 
+    price :99
+})
+
+const myNewArray = [200 , 300 , 400 , 500];
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
