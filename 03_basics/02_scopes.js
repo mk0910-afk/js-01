64728d61c19770481b4f4ca9eb488f5d5b44c2c7
mk0910-afk <152643948+mk0910-afk@users.noreply.github.com>
2025-04-04 21:35:40 +0000

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,7 +2,23 @@ let a = 10
 const b = 20
 var c = 30
 
+array = new Array;
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+
+//nested function , loop ,if else ,, concept of closure 
+
+// interesting
+
+addone(5)
+
+function addone(num) {
+    return num +1;
+}
+addTwo(5) //it will give error here 
+const addTwo = function(num1){
+    return num1 +2;
+}
+
